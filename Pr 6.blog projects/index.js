a5cb53be69774b16fd72c288490400467b907fae
MeetVadodariya <meetvadodariya9825@gmail.com>
2024-11-30 11:48:20 +0530

    add

---
+++ Pr 6.blog projects/index.js
@@ -0,0 +1,30 @@
+const express=require('express')
+
+const port=9000
+
+const app=express()
+
+app.use(express.urlencoded())
+
+app.set('view engine', 'ejs')
+
+const cookieParser = require('cookie-parser');
+
+let path=require('path')
+
+app.use(cookieParser())
+
+app.use('/',express.static(path.join(__dirname,'/public')))
+
+app.use('/uploads',express.static(path.join(__dirname,'uploads')))
+
+app.use('/',require('./routes/indexroutes'))
+
+const db = require('./config/db')
+
+app.listen(port,(err)=>{
+    if (err) {
+        console.log(err);
+    }
+    console.log("server is runing",port);
+})
